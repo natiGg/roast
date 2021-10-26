4e1d580978383ef5611b803be55299f0fe215270
natiGg <nathangere12@gmail.com>
2021-10-26 10:40:20 -0700

    Initial'

---
+++ roast-front/components/Feature.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="mx-auto container px-4 xl:px-0 py-10 xl:py-40">
+    <div class="">
+      <div class="flex flex-wrap items-center">
+        <div class="lg:w-2/5 w-full">
+          <img
+            class="w-full"
+            src="https://cdn.tuk.dev/assets/templates/prodify/solution.png"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <div class="lg:w-3/5 w-full">
+          <div class="lg:pl-8 py-2 text-color">
+            <h1 class="text-3xl xl:text-6xl leading-snug f-f-l font-black">
+              Gather data to offer a bespoke solution
+            </h1>
+            <h2
+              class="
+                text-xl
+                lg:text-3xl
+                leading-7
+                lg:leading-10
+                f-f-r
+                py-4
+                lg:py-8
+              "
+            >
+              Here at Globex we take special care of what your organization
+              needs instead of selling you a mass market tool that takes a one
+              size fits all approach. I personally review each and every client
+              business and oversee the team that tailores a solution
+            </h2>
+            <div class="flex items-center">
+              <div
+                class="flex items-center justify-center rounded-full w-16 h-16"
+              >
+                <img
+                  src="https://cdn.tuk.dev/assets/templates/prodify/profile.png"
+                  alt=""
+                />
+              </div>
+              <div class="pl-3">
+                <h3 class="f-f-l text-xl lg:text-3xl">Rachel Dawson</h3>
+                <h4 class="f-f-r text-lg pt-2">CEO, Globex Workspaces</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ roast-front/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Navigation/>
+    <Hero/>
+    <Feature/>
+    <Footer/>
+  </div>
+
+</template>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+import Hero from '@/components/Hero.vue'
+import Footer from '@/components/Footer.vue'
+import Feature from '@/components/Feature.vue'
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    Navigation,
+    Hero,
+    Feature,
+    Footer
+  }
+}
+</script>
